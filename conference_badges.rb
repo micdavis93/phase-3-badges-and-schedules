5e5b3d28c0ff07196a7ac1c86a3555350c4dc312
Michael Davis <micdavis93@gmail.com>
2022-12-14 11:27:19 -0700

    commit tree 85f1cffda67a6f5b298595e029828f31833d11f0

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    names.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(names)
+    attendees.map.with_index(1) do |name, room_num|
+        "Hello, #{name}! You'll be assigned to room #{room_num}!"
+    end
+end
+
+def printer(attendees)
+    puts batch_badge_creator(attendees)
+    puts assign_rooms(attendees)
+end
